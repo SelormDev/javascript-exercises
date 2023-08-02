38ec046500e10f49b6583d445e9b52e5a585baa8
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-08-02 20:32:42 +0000

    Solved exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (word, number) {
+  if (number < 0) return `ERROR`;
+  let str = "";
+  for (let i = 0; i < number; i++) str += word;
+  return str;
 };
 
 // Do not edit below this line
