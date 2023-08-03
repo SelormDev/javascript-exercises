da93d38a489def606172ed5298d2096d229fe609
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-08-03 08:54:56 +0000

    Solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (strings) {
+  let splittedStrings = strings.split("");
+  let newString = "";
+  for (let i = 0; i < strings.length; i++) {
+    newString += splittedStrings[splittedStrings.length - 1];
+    splittedStrings.pop();
+  }
+  return newString;
 };
 
 // Do not edit below this line
