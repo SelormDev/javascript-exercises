86d159acba0a0735cd17015ad330f6a7f4c57847
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-08-14 18:14:35 +0000

    Solved test01 in removeFromArray exerise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function () {};
+let removeFromArray = function (array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      array.splice(i, 1);
+      return array;
+    }
+  }
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
