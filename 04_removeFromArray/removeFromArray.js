a0fb26e464f2c26ea7e602f12139be978d9e1b44
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-08-17 15:42:03 +0000

    Solved all tests in removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,12 @@
-let removeFromArray = function (array, element) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === element) {
-      array.splice(i, 1);
-      return array;
+let removeFromArray = function (array, ...elements) {
+  for (let i = 0; i < elements.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (elements[i] === array[j]) {
+        array.splice(j, 1);
+      }
     }
   }
+  return array;
 };
 
 // Do not edit below this line
