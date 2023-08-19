e391579d8889c21a9c156a6bd5a01a0333b5d490
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-08-19 20:46:10 +0000

    Solved leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const centuryLeapYear = year % 400 === 0;
+  const nonCenturyLeapYear = year % 4 === 0 && !(year % 100 === 0);
+  return centuryLeapYear || nonCenturyLeapYear;
 };
 
 // Do not edit below this line
